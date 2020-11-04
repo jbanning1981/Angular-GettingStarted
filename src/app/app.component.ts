adeb508633fcc82b7cc7c56ec29fcc19f4d297ca
Jim B <jbanning1981@users.noreply.github.com>
2020-11-03 23:56:19 -0600

    added hide/show logic for product table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'store-root',
   template: `
+      <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <div class="navbar-brand">
+          ACME 
+        </div>
+      </nav>  
     <div class="container-fluid">
-      <nav class="nav">
-        
-      </nav>
       <pm-products></pm-products>
     </div>      
   `
